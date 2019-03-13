1365e55bd2c4a4a3b61ee70daacf9dbe270a2036
Swapnil Agarwal <swapnil.a@browserstack.com>
2019-03-13 15:05:00 +0530

    updated packages and test cases

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,3 @@
-test('isOdd()', function() {
-    ok(isOdd(1), 'One is an odd number');
-    ok(isOdd(3), 'Three is an odd number');
-    ok(isOdd(0), 'Zero is not odd number');
+QUnit.test("isOdd()", function( assert ) {
+  assert.ok(isOdd(1), 'One is an odd number');
 });
